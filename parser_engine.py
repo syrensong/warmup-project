1fac954cf027bb9aaf5b2fd3f98e848833ae8825
Zachary <zbelkin@uvm.edu>
2026-02-02 14:34:45 -0500

    made the outline for the rest of the query functions

--- parser_engine.py
+++ parser_engine.py
@@ -23,4 +23,34 @@ def doQuery (column, city="none", operand="none", quantity=0):
         elif column == "rank":
             doNumRankQuery(operand, quantity)
     else:
-        Invalid query message
+        print("Invalid Query Message")
+
+def doCityPopulationQuery(city):
+    return city
+
+def doCityWhereQuery(city):
+    return city
+
+def doCityStateQuery(city):
+    return city
+
+def doCityAreaQuery(city):
+    return city
+
+def doCityRankQuery(city):
+    return city
+
+def doCityBigQuery(city):
+    return city
+
+def doNumPopulationQuery(operand, quantity):
+    return quantity
+
+def doNumWageQuery(operand, quantity):
+    return quantity
+
+def doNumAreaQuery(operand, quantity):
+    return quantity
+
+def doNumRankQuery(operand, quantity):
+    return quantity
