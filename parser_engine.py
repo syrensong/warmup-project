5c1ef669441f8c5e43bc7e2804a4c118b764804c
Zachary <zbelkin@uvm.edu>
2026-02-06 16:55:58 -0500

    Simplified Help command

--- parser_engine.py
+++ parser_engine.py
@@ -90,10 +90,10 @@ def getHelp():
             "\tRANK “City”: returns the rank of the chosen city\n"
             "\tBIG “City”: returns if the chosen city is big\n"
         "Number Commands: <, <=, =, >=, >\n"
-            "\tPOPULATION >, <, = #: returns cities with a population >,<,= the given number\n"
-            "\tWAGE >, <, = #: returns cities with a living wage >,<,= the given number\n"
-            "\tAREA >, <, = #: returns cities with an area >,<,= the given number\n"
-            "\tRANK >, <, = #: returns cities with a rank >,<,= the given number\n"
+            "\tPOPULATION <=> #: returns cities with a population >,<,= the given number\n"
+            "\tWAGE <=> #: returns cities with a living wage >,<,= the given number\n"
+            "\tAREA <=> #: returns cities with an area >,<,= the given number\n"
+            "\tRANK <=> #: returns cities with a rank >,<,= the given number\n"
             )
 while True:
     user_input = input("Please choose your command between WHERE, POPULATION, STATE, AREA, RANK, BIG, NUMBER,HELP: ")
@@ -105,7 +105,7 @@ while True:
         city = "none"
         operator = user_input.split()[1]
         num = int(user_input.split()[2])
-    doQuery(column, city,operator, num)
+    doQuery(column, city, operator, num)
 
 
 main()
