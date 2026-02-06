62026dce2a3a83e4e82e2fc58197797eb2830159
Varun Vijay <vvijay@uvm.edu>
2026-02-06 14:13:15 -0500

    working with fb engine

--- firebase_engine.py
+++ firebase_engine.py
@@ -6,3 +6,6 @@ from firebase_admin import firestore
 cred = credentials.Certificate('/Users/laylamusallam/Downloads/CS3050/warmup-project-ea50f-firebase-adminsdk-fbsvc-29b43b0a28.json')
 app = firebase_admin.initialize_app(cred)
 db = firestore.client()
+
+cities = db.child("Cities").get()
+print(cities.val())
